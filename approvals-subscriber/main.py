c152ed061255b7dd4a16042b75422fc237595f73
axspireai <axspireai@gmail.com>
2025-09-09 18:42:57 +0530

    code changes

--- approvals-subscriber/main.py
+++ approvals-subscriber/main.py
@@ -13,7 +13,7 @@ async def pubsub_push(request: Request):
     data = base64.b64decode(pubsub_message["data"]).decode("utf-8")
     attributes = pubsub_message.get("attributes", {})
 
-    print("📩 Received message:", data, attributes)
+    print(" Received message:", data, attributes)
 
     # Later: write to Firestore
     return {"status": "processed"}
